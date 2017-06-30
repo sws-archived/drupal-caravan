ab0696a0f525bf0dfea0268f8e3f0ba9948056f8
kbrownell <kbrownell@users.noreply.github.com>
2017-06-30 14:43:51 -0700

    Run DrupalVM playbook with a caravan playbook (#1)
    * Add SSH and rename su-sws
    
    * Forwarding ssh keys
    
    * Updating ssh forwarding per sfeng's recommendations
    
    * Copy keys to container
    
    * Add key to ssh-agent
    
    * Pass in known_host approval
    
    * Use keyscan to save list of known hosts
    
    * Restructuring config files
    
    * Change key to 600
    
    * Check if a symlink exists before creating a new one
    
    * Initial file structure for running with ansible
    
    * Run drupalvm playbook from caravan
    
    * Run DrupalVM playbook with caravan config
    
    * Removing ssh config and hosts

--- setup-caravan.sh
+++ setup-caravan.sh
@@ -4,18 +4,9 @@
 # For example, /var/www/se3_blt
 
 # Setup config file paths
-BLT_PROJECT_CONFIG="vendor/kbrownell/drupal-caravan/config/project.local.yml"
-BEHAT_LOCAL_CONFIG="vendor/kbrownell/drupal-caravan/config/behat.local.yml"
-BEHAT_DEFAULT_CONFIG="vendor/kbrownell/drupal-caravan/config/behat.yml"
-DRUPALVM_CONFIG="vendor/kbrownell/drupal-caravan/config/drupalvm.config.yml"
-DRUSH_ALIAS="vendor/kbrownell/drupal-caravan/config/aliases.drushrc.php"
-
-# Copy config files into place
-cat $BLT_PROJECT_CONFIG >> blt/project.local.yml
-cat $BEHAT_LOCAL_CONFIG >> tests/behat/behat.local.yml
-cat $BEHAT_DEFAULT_CONFIG >> tests/behat/behat.yml
-cat $DRUPALVM_CONFIG >> vendor/geerlingguy/drupal-vm/config.yml
-cat $DRUSH_ALIAS >> drush/site-aliases/aliases.drushrc.php
+BEHAT_DEFAULT_CONFIG="vendor/su-sws/drupal-caravan/config/behat.yml"
+DRUPALVM_CONFIG="vendor/su-sws/drupal-caravan/config/caravan.yml"
+DRUSH_ALIAS="vendor/su-sws/drupal-caravan/config/caravan.aliases.drushrc.php"
 
 # Bake a Docker container with Drupal VM.
 
@@ -65,6 +56,8 @@ else
     -v $PWD:$DRUPALVM_PROJECT_ROOT/:$volume_opts \
     -p $DRUPALVM_IP_ADDRESS:$DRUPALVM_HTTP_PORT:80 \
     -p $DRUPALVM_IP_ADDRESS:$DRUPALVM_HTTPS_PORT:443 \
+    -v $HOME/.ssh/acquia_id_rsa:/root/.ssh/id_rsa \
+    -v $HOME/.ssh/acquia_id_rsa.pub:/root/.ssh/id_rsa.pub \
     $OPTS \
     geerlingguy/docker-$DISTRO-ansible:latest \
     $INIT
@@ -80,40 +73,11 @@ docker exec --tty $DRUPALVM_MACHINE_NAME env TERM=xterm \
 
 status "Provisioning Drupal VM inside Docker container..."
 docker exec $DRUPALVM_MACHINE_NAME env TERM=xterm ANSIBLE_FORCE_COLOR=true \
-  ansible-playbook $DRUPALVM_PROJECT_ROOT/vendor/geerlingguy/drupal-vm/provisioning/playbook.yml
+  DRUPALVM_ENV=acquia \
+  ansible-playbook $DRUPALVM_PROJECT_ROOT/vendor/su-sws/drupal-caravan/provisioning/playbook.yml
 
 status "...done!"
 status "Visit the Drupal VM dashboard: http://$DRUPALVM_IP_ADDRESS:$DRUPALVM_HTTP_PORT"
-
-status "Install BLT alias and vim"
-docker exec $DRUPALVM_MACHINE_NAME /var/www/earth/vendor/acquia/blt/scripts/blt/install-alias.sh -y
-
-status "Installing Chrome 59.0.3071.104"
-docker exec $DRUPALVM_MACHINE_NAME sudo apt-get install libxss1 libappindicator1 libindicator7 vim wget -y
-docker exec $DRUPALVM_MACHINE_NAME wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# Unpackaging chrome returns an error because it is missing dependencies, which is not a problem,
-# We install them a moment later.  So adding || true to be sure this behavior does not quite the script.
-docker exec $DRUPALVM_MACHINE_NAME sudo dpkg -i google-chrome-stable_current_amd64.deb || true
-docker exec $DRUPALVM_MACHINE_NAME sudo apt-get -f install -y
-docker exec $DRUPALVM_MACHINE_NAME google-chrome --version
-
-status "Installing Chromedriver"
-docker exec $DRUPALVM_MACHINE_NAME wget https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
-docker exec $DRUPALVM_MACHINE_NAME unzip chromedriver_linux64.zip
-docker exec $DRUPALVM_MACHINE_NAME sudo mv -f chromedriver /usr/local/share/chromedriver
-docker exec $DRUPALVM_MACHINE_NAME sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
-docker exec $DRUPALVM_MACHINE_NAME sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
-status "Starting Selenium"
-docker exec $DRUPALVM_MACHINE_NAME service selenium start
-
-status "Installing Site, this make take a while"
-# Forcing this to resolve as true, so the script may continue.
-# Content is not able to be imported at the comment.
-docker exec $DRUPALVM_MACHINE_NAME sh -c "cd /var/www/earth && vendor/bin/blt local:setup" || true
-
-status "Running tests"
-docker exec $DRUPALVM_MACHINE_NAME sh -c "cd /var/www/earth/tests/behat && ../../vendor/bin/behat -p local --colors features"
 
 # Do not run in continuous integration environments
 if [ -z "$ENVIRONMENT" ]; then
